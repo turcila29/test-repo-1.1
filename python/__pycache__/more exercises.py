cc5cacb5b5766e0b1feed9dcfbe1879e17677361
Turc <turcilaandrei30@gmail.com>
2023-07-19 18:31:19 +0300

    new exercises

---
+++ python/__pycache__/more exercises.py
@@ -0,0 +1,74 @@
+#CREATED A NEW DEF AND SPLIT IT
+#CREATED A NEW LIST REVERSING THE WORDS
+#AND USED JOIN TO PUT THEM TOGETHER
+
+#str = "My name is Jessa"
+
+#def reversed_words(sentence):
+    #words = sentence.split(" ")
+
+    #new_word = [word[::-1] for word in words]
+
+
+    #res_str = " ".join(new_word)
+    #eturn res_str
+
+#print (reversed_words(str))
+
+
+#GOT THE LIST SIZE, ITERATE USEING LOOP CHECK IF NUMBER IS BIGGER THAN 50
+#DELETE THE NUMBER IF YES AND SHORTEN THE LIST
+#number_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+#i = 0
+
+#n = len(number_list)
+
+#while i < n:
+
+    #if number_list[i] > 50:
+
+        #del number_list[i]
+
+        #n = n - 1
+
+    #else:
+       # i = i + 1
+
+    #print(number_list)
+
+
+
+
+
+
+#ascii_dict = {'A': 65, 'B': 66, 'C': 67, 'D': 68}
+# in order to reverse the values, we use reverse mapping
+
+#reverse_dict = {value: key for key, value in ascii_dict.items()}
+#print(reverse_dict)
+
+
+
+#rows = 7
+#x = 0
+
+#for i in range(rows, 0, -1):
+ #   x += 1
+  #  for j in range(1, i + 1):
+   #     print(x, end=' ')
+    #print('\r')
+
+
+
+#def mixednames(x, y):
+ #   def fun_times(x, y):
+  #      return x +y
+    
+
+   # a = fun_times(x, y)
+    #return a + 'Developers'
+
+#result = mixednames('Emma', 'Kelly')
+#print(result)
+
