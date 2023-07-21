9da71ebd038ef3277b122afb610209046c7c6c3d
Turc <turcilaandrei30@gmail.com>
2023-07-21 13:04:39 +0300

    exericse

--- python/python tests/test_count.py
+++ python/python tests/test_count.py
@@ -1,9 +1,5 @@
 import count
-
-
 def test_count_zeros():
-    assert count.count ([0,0,0], 0) == 3
-
-
+    assert count.count([0,0,0], 0) == 3
 def test_count_string():
     assert count.count(["a","a","a"], "a") == 3
