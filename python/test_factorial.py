2415efad80d39a56398e350ccd7164e92f589d1f
Turc <turcilaandrei30@gmail.com>
2023-07-21 13:11:51 +0300

    changes

--- python/test_factorial.py
+++ python/test_factorial.py
@@ -1,6 +1,5 @@
 import factorial
-def test_fact_one():
-    assert factorial.fact(4)
-def test_fact_string():
-    assert factorial.fact("asd")
-    
+def test_fact():
+    assert factorial.fact(0) == 1
+    assert factorial.fact(24) == 620448401733239439360000
+    assert factorial.fact(6) == 720
