e4e7e4f1d41955d69f6b5fa9484914b66db7e4d0
Turc <turcilaandrei30@gmail.com>
2023-07-24 12:30:48 +0100

    new file

---
+++ python/24_07_23/gui_example.py
@@ -0,0 +1,19 @@
+import tkinter
+
+def strip_vowels():
+    global label
+    label.destroy()
+    label = tkinter.Label(root, text =  ''.join(char for char in string if char not in 'aeiouAEIOU'))
+    label.grod(row=2, column=1)
+root = tkinter.Tk()
+
+label = tkinter.Label(root, text = "")
+input1 = tkinter.Entry(root)
+input1.grid(row=1, column=2)
+
+submit = tkinter.Button(root, test = "strip vowels", command=strip_vowels)
+
+
+if __name__ == '__main__':
+    root.mainloop()
+    
