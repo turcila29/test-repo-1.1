a6a59b97d3f1cf70b3c3142443056b57f4ff3862
Turc <turcilaandrei30@gmail.com>
2023-07-26 11:15:14 +0100

    exercises

---
+++ python/25_07_23/exercise.py
@@ -0,0 +1,29 @@
+def maxlen(var1, var2):
+
+
+    if len(var1) > len(var2):
+        print(var1 + " is the winner")
+
+    elif len(var1) == len(var2):
+        print(var1 + " " + var2)
+    
+    else:
+        print(var2 + " is the winner")
+
+        
+  
+        
+    
+maxlen("dogs", "cats")
+
+
+
+
+
+
+
+
+
+ 
+
+    
