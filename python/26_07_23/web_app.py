cd7bc5550df31765f9032a73dc7f79905a581207
Turc <turcilaandrei30@gmail.com>
2023-07-26 12:38:57 +0100

    new web app

--- python/26_07_23/web_app.py
+++ python/26_07_23/web_app.py
@@ -2,17 +2,17 @@ from flask import Flask
 import random
 
 app = Flask(__name__)
-
+First_name = "jay", "andrei", "ramon", "emanuel", "rebeca", "george", "alex", "david"
+Last_name = "popescu", "gosling", "evans", "pratt", "hemsworth", "dirver", "roger", "bleacher", "ratatouille"
+First = random.choice(First_name)
+Last = random.choice(Last_name)
 
 @app.route("/")
 
 def name_gen():
-
-    First_name = "jay", "andrei", "ramon", "emanuel", "rebeca", "george", "alex", "david"
-    Last_name = "popescu", "gosling", "evans", "pratt", "hemsworth", "dirver", "roger", "bleacher", "ratatouille"
-    First = random.choice(First_name)
-    Last = random.choice(Last_name)
+   
+    
     return(First + " " + Last)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+        app.run(debug = True)
