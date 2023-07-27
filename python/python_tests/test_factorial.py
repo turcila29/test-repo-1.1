b027e3876b97264eb868f7f651af4798d85e4bdc
Turc <turcilaandrei30@gmail.com>
2023-07-27 16:20:02 +0100

    webhook setting

--- python/python_tests/test_factorial.py
+++ python/python_tests/test_factorial.py
@@ -3,3 +3,5 @@ def test_fact():
     assert factorial.fact(0) == 1
     assert factorial.fact(24) == 620448401733239439360000
     assert factorial.fact(6) == 720
+
+#this is a comment for the webhook
