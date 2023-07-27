3d70e191a1b3fd6a86d0f9f670756eb152b23e6f
Turc <turcilaandrei30@gmail.com>
2023-07-27 15:19:12 +0100

    changs

--- python/25_07_23/test_exercise.py
+++ python/25_07_23/test_exercise.py
@@ -1,7 +1,9 @@
 import pytest
 from exercise import maxlen
 
-
 def test_one():
-    assert maxlen("tiger", "computer") == "computer"
-    assert maxlen("egg", "building") == "building"
+    assert maxlen.one("hi","hello") == "hello"
+    assert maxlen.one("three", "two") == "three"
+    assert maxlen.one("three", "hello") == "three hello"
+    assert maxlen.one("echo", "print") == "print"
+    assert maxlen.one("fire","rib") == "fire"
