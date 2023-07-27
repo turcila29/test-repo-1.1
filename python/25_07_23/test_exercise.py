cda91aec08a50349a5cd8f3860e0bdd67ba8be04
Turc <turcilaandrei30@gmail.com>
2023-07-27 15:09:25 +0100

    change

--- python/25_07_23/test_exercise.py
+++ python/25_07_23/test_exercise.py
@@ -3,6 +3,5 @@ from exercise import maxlen
 
 
 def test_one():
-    assert maxlen.one("white", "orange") == "orange"
     assert maxlen.one("tiger", "computer") == "computer"
     assert maxlen.one("egg", "building") == "building"
