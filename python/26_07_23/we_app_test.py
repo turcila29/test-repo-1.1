dd2a284d6def6d2f07f48ddbf01586ef4851755b
Turc <turcilaandrei30@gmail.com>
2023-07-28 14:41:26 +0100

    webhook testing

--- python/26_07_23/we_app_test.py
+++ python/26_07_23/we_app_test.py
@@ -4,3 +4,5 @@ from web_app import name_gen, First_name, Last_name
 def test_name_test():
     assert name_gen().split(" ")[0] in First_name 
     assert name_gen().split(" ")[1] in Last_name
+
+    #this is a test for the webhook
