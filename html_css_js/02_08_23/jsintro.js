3eae6be29e465cbe512c1a66676129629248e3b2
Turc <turcilaandrei30@gmail.com>
2023-08-02 16:43:30 +0100

    new exercise

---
+++ html_css_js/02_08_23/jsintro.js
@@ -0,0 +1,17 @@
+console.log("Andrei");
+console.log("Turcila");
+console.log("Romania");
+console.log("Leo");
+
+
+let carmodel = "A5";
+const carmaker = "Audi";
+
+console.log(`My favourite care is ${carmodel} and the model is ${carmaker}`); 
+
+console.log("this is %c a beautiful text", "color: orange; font-family: fantasy; text: bold; background-color: black; padding: 10px; ");
+
+
+
+
+
